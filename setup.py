fe7cce7cd1d9dbe83487b9683d5660762db33b45
“prodip1023” <prodip1023@gmail.com>
2025-07-20 12:39:35 +0530

    project-structure-day3

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Document-Portal',
+    version='0.1.0',
+    author='Prodip Sarkar',
+    long_description ='''I have built an intelligent document portal that allows users to upload single or multiple documents for seamless interaction. \
+    Users can chat with the uploaded documents, ask questions, and receive instant insights. \ 
+    The system supports real-time analysis, making document review efficient and user-friendly. \
+    It’s designed for professionals who need quick understanding of large or complex files. \
+    The portal combines AI-powered search with natural language conversation for an intuitive experience''',
+
+    packages=find_packages(),
+)
+    
